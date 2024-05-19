ba74bf121ca8153bd878f23f6717b990bcbe88d6
Michael <michael.schneider_uh@proton.me>
2024-05-19 00:08:43 -0500

    Update NPC's_ID's.lua

--- NPC's_ID's.lua
+++ NPC's_ID's.lua
@@ -1,1 +1,6 @@
-
+Name			CharaID		Numeric ID	Location
+?			ch310087	424542582	Wandering
+Abba			ch300280	1652338685	Flambearer Palace
+Abdul			ch300348	1333279266	Wandering
+Abel			ch300328	1650521133	Harve
+Abigail			ch300118	2148990346	Melve
