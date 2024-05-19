16a88984e0c20d529ae4fefdb141be33e1eb3e1f
Michael <michael.schneider_uh@proton.me>
2024-05-19 00:08:51 -0500

    Update NPC's_ID's.lua

--- NPC's_ID's.lua
+++ NPC's_ID's.lua
@@ -4,3 +4,9 @@ Abba			ch300280	1652338685	Flambearer Palace
 Abdul			ch300348	1333279266	Wandering
 Abel			ch300328	1650521133	Harve
 Abigail			ch300118	2148990346	Melve
+Abraham			ch300199	546316533	Vernworth
+Accardo			ch300137	2464077077	Borderwatch Outpost
+Achille			ch300067	952698873	Vernworth
+Acilia			ch300765	748874459	Excavation Site
+Adelina			ch310059	815535650	Vernworth
+Adib			ch300527	1745683823	Bakbattahl
